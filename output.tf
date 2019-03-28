19f9bb429533b060b85b170b8dae8d93a7881f69
U-SVA\phermann <phermann@TPPHERMANN.sva.de>
2019-03-28 08:57:55 +0100

    added scond machine

--- output.tf
+++ output.tf
@@ -1,7 +1,7 @@
-output "webserver_public_instance_ip" {
-  value = ["${aws_instance.webserver.public_ip}"]
-}
-
-output "databaseserver_public_instance_ip" {
-  value = ["${aws_instance.databaseserver.public_ip}"]
-}
+# output "webserver_public_instance_ip" {
+#   value = ["${aws_instance.webserver.public_ip}"]
+# }
+#
+# output "databaseserver_public_instance_ip" {
+#   value = ["${aws_instance.databaseserver.public_ip}"]
+# }
